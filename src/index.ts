1ad41eab159b2e457cc05dd8d3d77e34857a70df
samsostudios <sam@samsostudios.com>
2024-10-29 14:44:09 -0600

    added scroll scale and media slider moudules ++ various util modules

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { loadComponent } from '$utils/loadComponent';
+import { smoothScroll } from '$utils/smoothScroll';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   console.log('/// Oakley ///');
 
+  smoothScroll();
+
   loadComponent('[data-hover-video]', () => import('$components/hoverVideos'));
+  loadComponent('.overview_component', () => import('$components/scrollScale'));
+  loadComponent('.slider_component', () => import('$components/mediaSlider'));
 });
