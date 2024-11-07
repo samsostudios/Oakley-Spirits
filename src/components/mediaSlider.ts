d0ee113311fdbfe34d859d0c60e2a084ad2cf519
samsostudios <sam@samsostudios.com>
2024-11-07 13:01:31 -0700

    shop slider progress ++ media slider updates

--- src/components/mediaSlider.ts
+++ src/components/mediaSlider.ts
@@ -10,6 +10,8 @@ export const mediaSlider = () => {
     private currentOffset: number;
     private pagePadding: number;
     private imageSpacing: number;
+    private labelCurrent: HTMLElement;
+    private labelTotal: HTMLElement;
 
     constructor() {
       this.sliderTrack = document.querySelector('.slider_track') as HTMLElement;
@@ -18,6 +20,10 @@ export const mediaSlider = () => {
       );
       this.nextButton = document.querySelector('.button.is-icon.slider-next') as HTMLElement;
       this.prevButton = document.querySelector('.button.is-icon.slider-prev') as HTMLElement;
+
+      this.labelCurrent = document.querySelector('.slider_i-text.is-current') as HTMLElement;
+      this.labelTotal = document.querySelector('.slider_i-text.is-total') as HTMLElement;
+
       this.curIndex = 0;
       this.currentOffset = 0;
       this.pagePadding = 16;
@@ -31,26 +37,33 @@ export const mediaSlider = () => {
     private setInitialOffset() {
       // Initialize the offset with the page padding
       this.currentOffset = 1;
+      this.updateTextElements();
+      this.labelTotal.textContent = this.formatNumber(this.sliderImages.length);
     }
 
     private setListeners() {
       this.nextButton.addEventListener('click', () => this.advance());
       this.prevButton.addEventListener('click', () => this.regress());
+      window.addEventListener(
+        'resize',
+        this.debounce(() => this.resizeHandler(), 200)
+      );
     }
 
     private advance() {
       if (this.curIndex < this.sliderImages.length - 1) {
         this.currentOffset += this.sliderImages[this.curIndex].offsetWidth + this.imageSpacing;
         this.curIndex += 1;
+        this.updateTextElements();
         this.updateSliderPosition();
       }
     }
 
     private regress() {
       if (this.curIndex > 0) {
-        // Move to the previous image and update offset
         this.curIndex -= 1;
-        this.currentOffset -= this.sliderImages[this.curIndex].offsetWidth;
+        this.currentOffset -= this.sliderImages[this.curIndex].offsetWidth + this.imageSpacing;
+        this.updateTextElements();
         this.updateSliderPosition();
       }
     }
@@ -61,6 +74,33 @@ export const mediaSlider = () => {
         duration: 0.5,
         ease: 'power2.out',
       });
+    }
+
+    private updateTextElements() {
+      // Update current slide number
+      this.labelCurrent.textContent = this.formatNumber(this.curIndex + 1);
+    }
+
+    private formatNumber(num: number): string {
+      return num < 10 ? `0${num}` : `${num}`;
+    }
+
+    private resizeHandler() {
+      // Reset the current offset and re-calculate based on the current index
+      this.currentOffset = 0;
+      for (let i = 0; i < this.curIndex; i++) {
+        this.currentOffset += this.sliderImages[i].offsetWidth + this.imageSpacing;
+      }
+      this.updateSliderPosition();
+    }
+
+    // Utility function to debounce the resize event
+    private debounce(func: (...args: any[]) => void, wait: number): (...args: any[]) => void {
+      let timeout: number | undefined;
+      return (...args: any[]) => {
+        clearTimeout(timeout);
+        timeout = window.setTimeout(() => func(...args), wait);
+      };
     }
   }
   new MediaSlider();
--- src/components/shopSlider.ts
+++ src/components/shopSlider.ts
@@ -2,13 +2,15 @@ import gsap from 'gsap';
 
 export const shopSlider = () => {
   class ShopSlider {
-    // private component: HTMLElement;
     private imageFeed: HTMLImageElement[];
     private bgImages: HTMLImageElement[];
     private previewWraps: HTMLImageElement[];
     private previewImages: HTMLImageElement[];
+    private pIndicators: HTMLImageElement[];
     private currentIndex: number;
     private rotationInterval: number;
+    private labelCurrent: HTMLElement;
+    private labelTotal: HTMLElement;
 
     constructor() {
       this.imageFeed = [...document.querySelectorAll('.slider_data-img')].map(
@@ -23,39 +25,89 @@ export const shopSlider = () => {
       this.previewImages = [...document.querySelectorAll('.slider_preview-img')].map(
         (item) => item as HTMLImageElement
       );
+      this.pIndicators = [...document.querySelectorAll('.slider_preview-indicator')].map(
+        (item) => item as HTMLImageElement
+      );
+
+      this.labelCurrent = document.querySelector(
+        '.slider_i-text.is-shop.is-current'
+      ) as HTMLElement;
+      this.labelTotal = document.querySelector('.slider_i-text.is-shop.is-total') as HTMLElement;
+
+      console.log(this.labelTotal);
 
       this.currentIndex = 0;
-
-      console.log('images', this.bgImages);
+      this.rotationInterval = 5000;
 
       this.setupImages();
-      this.controlSlider();
+      this.startRotation();
     }
 
     private setupImages() {
       for (let i = 0; i < this.imageFeed.length; i++) {
         const item = this.imageFeed[i];
-        console.log('HERE', item);
         this.bgImages[i].src = item.src;
-        // if(i ===  images)
-        // this.previewImages[i].src = item.src;
+        this.previewImages[i].src = item.src;
+        if (i !== 0) {
+          gsap.set(this.bgImages[i], { opacity: 0 });
+        }
+
+        this.labelTotal.textContent = this.formatNumber(this.bgImages.length);
       }
     }
 
-    private controlSlider() {
-      this.rotationInterval = setInterval(() => {
+    private startRotation() {
+      this.updateIndicator(this.currentIndex);
+      this.updateImages(this.bgImages.length - 1, this.currentIndex);
+      this.setActivePreview(this.currentIndex);
+      this.updateTextElements();
+
+      setInterval(() => {
         const previousIndex = this.currentIndex;
         this.currentIndex = (this.currentIndex + 1) % this.bgImages.length;
+
+        this.updateIndicator(this.currentIndex);
         this.updateImages(previousIndex, this.currentIndex);
-      }, 5000);
+        this.setActivePreview(this.currentIndex);
+        this.updateTextElements();
+
+        gsap.set(this.pIndicators[previousIndex], { x: '0%' });
+      }, this.rotationInterval);
     }
 
     private updateImages(prevIndex: number, currentIndex: number) {
       gsap.to(this.bgImages[prevIndex], { opacity: 0, duration: 1 });
       gsap.to(this.bgImages[currentIndex], { opacity: 1, duration: 1 });
+    }
 
-      //   gsap.to(this.previewImages[prevIndex], { opacity: 0, duration: 1 });
-      //   gsap.to(this.previewImages[currentIndex], { opacity: 1, duration: 1 });
+    private updateIndicator(currentIndex: number) {
+      gsap.fromTo(
+        this.pIndicators[currentIndex],
+        { x: '0%' },
+        { duration: this.rotationInterval / 1000, x: '100%', ease: 'linear' }
+      );
+    }
+
+    private setActivePreview(currentIndex: number) {
+      this.previewWraps.forEach((wrap, index) => {
+        if (index !== currentIndex) {
+          wrap.classList.remove('is-active');
+          gsap.to(wrap, { borderColor: 'transparent', duration: 0.5 });
+        }
+      });
+
+      // Add 'is-active' to the current preview and animate it
+      this.previewWraps[currentIndex].classList.add('is-active');
+      gsap.to(this.previewWraps[currentIndex], { borderColor: 'currentColor', duration: 0.5 });
+    }
+
+    private updateTextElements() {
+      // Update current slide number
+      this.labelCurrent.textContent = this.formatNumber(this.currentIndex + 1);
+    }
+
+    private formatNumber(num: number): string {
+      return num < 10 ? `0${num}` : `${num}`;
     }
   }
 
